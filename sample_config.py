02c353a69e7c80dcb8a1c53f18f6cd70dc2149a5
playkuttan <76476634+playkuttan@users.noreply.github.com>
2021-01-17 22:31:35 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -15,8 +15,8 @@ class Config(object):
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     # Telegram maximum file upload size
     MAX_FILE_SIZE = 50000000
-    TG_MAX_FILE_SIZE = 1952864000
-    FREE_USER_MAX_FILE_SIZE = 50000000
+    TG_MAX_FILE_SIZE = 2097152000
+    FREE_USER_MAX_FILE_SIZE = 2097152000
     # chunk size that should be used with requests
     CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
     # default thumbnail to be used in the videos
