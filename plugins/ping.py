94fcd1b6e0f68a2b26b857bc5380969694c2f3b9
playkuttan <76476634+playkuttan@users.noreply.github.com>
2021-01-18 17:53:01 +0530

    Update ping.py

--- plugins/ping.py
+++ plugins/ping.py
@@ -2,7 +2,6 @@ import asyncio
 from datetime import datetime
 
 
-@pyrogram.Client.on_message(pyrogram.Filters.command(["ping"]))
 @run_async
 def ping(update, context):
     start_time = int(round(time.time() * 1000))
