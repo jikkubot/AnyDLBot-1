ab8434b5dde5d9a6a6bcafff736272ddfe0c3180
playkuttan <76476634+playkuttan@users.noreply.github.com>
2021-01-20 12:21:12 +0530

    Update ping.py

--- plugins/ping.py
+++ plugins/ping.py
@@ -1,9 +1,11 @@
 import asyncio
 from datetime import datetime
 from pyrogram import Client, Filters
+from helper_funcs.chat_base import TRChatBase
 
 
 @Client.on_message(Filters.command(["ping"]))
+TRChatBase(update.from_user.id, update.text, "/help")
 async def ping(client, message):
     start = datetime.now()
     end = datetime.now()
