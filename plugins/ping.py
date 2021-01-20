6281b593eb0e6ff73ef05a51a71d2c4f765ccc58
playkuttan <76476634+playkuttan@users.noreply.github.com>
2021-01-20 10:16:06 +0530

    Add files via upload

---
+++ plugins/ping.py
@@ -0,0 +1,7 @@
+from pyrogram import Client, Filters
+
+
+@Client.on_message(Filters.command(["ping"]))
+async def start(client, message):
+    helptxt = f"Send me YouTube Video Link I will upload to Telegram! \n\n(No PlayList Supported!)"
+    await message.reply_text(helptxt)
