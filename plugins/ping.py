e9b3ab3eac2a98d3f2c3e68b52ace89d3e2bd63f
playkuttan <76476634+playkuttan@users.noreply.github.com>
2021-01-20 12:43:18 +0530

    Update ping.py

--- plugins/ping.py
+++ plugins/ping.py
@@ -1,21 +1,11 @@
-import logging
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
-
-import os
-import shutil
-import subprocess
 import time
 import asyncio
 from datetime import datetime
 from pyrogram import Client, Filters
-from helper_funcs.chat_base import TRChatBase
 
 
-@Client.on_message(Filters.command(["ping"]))
+@Client.on_message(Filters.command(["ping", "ping@xploaderprobot]))
 async def ping(client, message):
-TRChatBase(update.from_user.id, update.text, "/help")
     start = datetime.now()
     end = datetime.now()
     ms = (end - start).microseconds / 1000
