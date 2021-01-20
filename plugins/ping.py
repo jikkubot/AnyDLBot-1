c8518ab311af74deab16b6df0f9f6e380be45eab
playkuttan <76476634+playkuttan@users.noreply.github.com>
2021-01-20 11:46:35 +0530

    Update ping.py

--- plugins/ping.py
+++ plugins/ping.py
@@ -6,7 +6,7 @@ from pyrogram import Client, Filters
 @Client.on_message(Filters.command(["ping"]))
 async def ping(client, message):
     start = datetime.now()
-    event = await reply_text(event, "Pong!")
+    event = await message.reply_text(event, "Pong!")
     end = datetime.now()
     ms = (end - start).microseconds / 1000
     await message.reply_text("Pong!\n`{}`".format(ms))
