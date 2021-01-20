c389435e3e9a7fe1395d11d21aa31b92280925d2
playkuttan <76476634+playkuttan@users.noreply.github.com>
2021-01-20 22:35:17 +0530

    Update ping.py

--- plugins/ping.py
+++ plugins/ping.py
@@ -5,11 +5,11 @@ from pyrogram import Client, Filters
 
 
 @Client.on_message(Filters.command(["ping", "ping@xploaderprobot"]))
-async def ping(client, message):
+async def ping(client, message, bot):
     start_time = int(round(time.time() * 1000))
     event = await message.reply_text("Starting Ping")
     end_time = int(round(time.time() * 1000))
-    await message.edit_text(f'{end_time - start_time} ms', event)
+    await bot.edit_message_text(f'{end_time - start_time} ms', event)
     
     
     
