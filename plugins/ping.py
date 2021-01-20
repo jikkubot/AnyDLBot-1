e3f05542bd30a89878bcf56914f35589f0726a2f
playkuttan <76476634+playkuttan@users.noreply.github.com>
2021-01-20 11:27:10 +0530

    Update ping.py

--- plugins/ping.py
+++ plugins/ping.py
@@ -1,7 +1,14 @@
+import asyncio
+from datetime import datetime
 from pyrogram import Client, Filters
 
 
 @Client.on_message(Filters.command(["ping"]))
 async def start(client, message):
-    helptxt = f"Send me YouTube Video Link I will upload to Telegram! \n\n(No PlayList Supported!)"
-    await message.reply_text(helptxt)
+    if event.fwd_from:
+        return
+    start = datetime.now()
+    event = await edit_or_reply(event, "Pong!")
+    end = datetime.now()
+    ms = (end - start).microseconds / 1000
+    await event.edit("Pong!\n`{}`".format(ms))
